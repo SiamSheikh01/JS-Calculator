78bee5ba8a2831cdd6ae1f7b4b47a8904897bb94
Mahmudur Rahman <130636898+SiamSheikh01@users.noreply.github.com>
2024-11-11 17:43:29 +0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,19 +2,21 @@ const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
-//Define function to calculate based on button clicked.
+//Define function to calculate.
+
 const calculate = (btnValue) => {
   display.focus();
   if (btnValue === "=" && output !== "") {
-    //If output has '%', replace with '/100' before evaluating.
+    //If output has '%', replace with '/100' .
+    
     output = eval(output.replace("%", "/100"));
   } else if (btnValue === "AC") {
     output = "";
   } else if (btnValue === "DEL") {
-    //If DEL button is clicked, remove the last character from the output.
+    
     output = output.toString().slice(0, -1);
   } else {
-    //If output is empty and button is specialChars then return
+   
     if (output === "" && specialChars.includes(btnValue)) return;
     output += btnValue;
   }
